d761c447be1e414542d40532fd0ca9ffd3945eb7
Mohamad Ripal Perdiansyah <mohamadripal001@gmail.com>
2023-10-06 22:18:57 +0700

    Initial Commit

--- webpack.common.js
+++ webpack.common.js
@@ -1,103 +1,108 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
-
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   entry: {
-    app: path.resolve(__dirname, 'src/scripts/index.js')
+    app: path.resolve(__dirname, 'src/scripts/index.js'),
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        test: /.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico)$/i,
+        type: 'asset',
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: path.resolve(__dirname, 'src/templates/index.html')
+      template: path.resolve(__dirname, 'src/templates/index.html'),
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/')
-        }
-      ]
+          to: path.resolve(__dirname, 'dist/assets'),
+          globOptions: {
+            ignore: ['**/resto-radar.png'],
+          },
+        },
+      ],
     }),
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, 'dist')]
+    new CleanWebpackPlugin(),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: './sw.bundle.js',
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) => url.href.startsWith('https://restaurant-api.dicoding.dev/'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'restaurant-api-v2',
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        },
+      ],
     }),
-    new FaviconsWebpackPlugin({
-      logo: path.resolve(__dirname, 'src/public/images/resto-radar-icon.svg'),
-      outputPath: 'assets/',
-      mode: 'webapp',
-      // devMode: 'webapp',
-      favicons: {
-        appName: 'RadarResto',
-        background: '#fff',
-        theme_color: '#fff',
-        icons: {
-          coast: false,
-          yandex: false,
-          windows: false,
-          appleStartup: false,
-          firefox: false,
-          android: false,
-          appleIcon: false,
-          favicons: true
-        }
-      }
-    })
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'server',
+    }),
   ],
   optimization: {
     minimizer: [
-      new TerserPlugin({}),
+      new TerserPlugin(),
       new CssMinimizerPlugin({
         minify: CssMinimizerPlugin.cssoMinify,
-        parallel: true
+        parallel: true,
       }),
       new ImageMinimizerPlugin({
         minimizer: {
           implementation: ImageMinimizerPlugin.imageminMinify,
           options: {
-            encodeOptions: {
-              mozjpeg: {
-                quality: 50
-              },
-              avif: {
-                quality: 50
-              },
-              png: {},
-              svg: {
-                quality: 50
-              },
-              gif: {},
-              jpg: {
-                quality: 50
-              }
+            mozjpeg: {
+              quality: 50,
+            },
+            avif: {
+              quality: 50,
+            },
+            webp: {
+              quality: 30,
+            },
+            png: {
+              quality: 10,
+            },
+            svg: {
+              quality: 20,
+            },
+            gif: {},
+            jpg: {
+              quality: 10,
             },
             plugins: [
-              ['mozjpeg', { quality: 50 }],
-              ['pngquant', { quality: [0.5, 0.5] }],
-              ['svgo', { quality: 50 }]
-            ]
-          }
-        }
-      })
-    ]
-  }
-}
+              ['mozjpeg', { quality: 20 }],
+              ['pngquant', { quality: [0.2, 0.2] }],
+              ['svgo', { quality: 20 }],
+            ],
+          },
+        },
+      }),
+    ],
+  },
+};
